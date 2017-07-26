2490813544d3f94a74a7466c84e4e3c6cfc54b45
Bangalger <geerman.figueroa@gmail.com>
2017-07-26 13:35:30 -0300

    Agregados y cambios
    article
    selectores jquery
    cambios css

--- assets/js/func.js
+++ assets/js/func.js
@@ -68,4 +68,9 @@ $(document).ready(function(){
         loop();
     }
     drawCanvas();
+
+    $("#change").click(function () {
+        $('header, section, footer').toggleClass("blue");
+    });
+
 });
